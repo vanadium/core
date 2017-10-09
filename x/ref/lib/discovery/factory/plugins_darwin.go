6e90617d3f22ad3f1f861004a026f112a799fe75
Razvan Musaloiu-E <razvan@musaloiu.com>
2017-10-09 10:41:19 -0700

    Remove the corebluetooth (#19)
    This code is macOS specific and susceptible to breakage with each Xcode
    release.

--- x/ref/lib/discovery/factory/plugins_darwin.go
+++ x/ref/lib/discovery/factory/plugins_darwin.go
@@ -7,20 +7,11 @@
 package factory
 
 import (
-	"v.io/v23/context"
-	"v.io/x/ref/lib/discovery/plugins/ble"
-	"v.io/x/ref/lib/discovery/plugins/ble/corebluetooth"
 	"v.io/x/ref/lib/discovery/plugins/mdns"
 )
 
 func init() {
-	bleFactory := func(ctx *context.T, _ string) (ble.Driver, error) {
-		return corebluetooth.New(ctx)
-	}
-	ble.SetDriverFactory(bleFactory)
-
 	pluginFactories = pluginFactoryMap{
 		"mdns": mdns.New,
-		"ble":  ble.New,
 	}
 }
