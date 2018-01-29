c1d7b3478f3eae266ff179bbc1a66f630233d526
Razvan Musaloiu-E <razvan@musaloiu.com>
2018-01-29 10:44:48 -0800

    Remove the syncbase assets for the debug browse (#27)
    This also removes a non-working link to Syncbase from `chrome.html`
    and updates the `gen_assets.sh` used to generate the `assets.go` to not
    depend on the jiri tool.

--- x/ref/services/debug/debug/browseserver/gen_assets.sh
+++ x/ref/services/debug/debug/browseserver/gen_assets.sh
@@ -5,24 +5,25 @@
 
 # Generate the assets.go source file, by running go-bindata.
 
-set -euf -o pipefail
+set -exuf -o pipefail
 
-# Install go-bindata
-jiri go install github.com/jteeuwen/go-bindata/...
+# Install go-bindata.
+export GOPATH=$(mktemp -d)
+mkdir ${GOPATH}/src
+go get github.com/jteeuwen/go-bindata/...
 
-# The cwd must be set here in order for paths start with "v.io/..."
 cd "$(dirname $0)"
 
 OUT="assets.go"
 TMP=$(mktemp "XXXXXXXXXX_assets.go")
 
 # Run go-bindata to generate the file to a tmp file.
-jiri run "${JIRI_ROOT}/third_party/go/bin/go-bindata" \
+"${GOPATH}/bin/go-bindata" \
     -o "${TMP}" -pkg browseserver -prefix "assets" \
     -nometadata -mode 0644 "assets/..."
 
 # Format the file and add the copyright header.
-jiri go fmt "${TMP}" > /dev/null
+go fmt "${TMP}" > /dev/null
 cat - "${TMP}" >  "${OUT}" <<EOF
 // Copyright 2016 The Vanadium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
@@ -30,5 +31,5 @@ cat - "${TMP}" >  "${OUT}" <<EOF
 
 EOF
 
-# Remove the tmp file.
-rm "${TMP}"
+# Remove the tmp file and dir.
+rm -rf "${TMP}" "${GOPATH}"
