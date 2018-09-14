9b55958793685e658f10b69dbdf3475306308d06
Cosmos Nicolaou <cnicolaou@grailbio.com>
2018-09-13 19:41:18 -0700

    use Infof at level 0

--- x/ref/runtime/internal/flow/conn/conn.go
+++ x/ref/runtime/internal/flow/conn/conn.go
@@ -497,7 +497,7 @@ func (c *Conn) healthCheckNewFlowLocked(ctx *context.T, timeout time.Duration) {
 			// they have been initialized. The simplest fix is to just
 			// initialize them here.
 			c.hcstate = c.newHealthChecksLocked(ctx, timeout)
-			vlog.Infof("initialzing health checks on first refresh")
+			vlog.VI(0).Infof("healthCheckNewFlowLocked: initializing health checks on first refresh: conn %p, timeout: %v", c, timeout)
 			return
 		}
 		if min := minChannelTimeout[c.local.Protocol]; timeout < min {
