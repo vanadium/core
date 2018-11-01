1740611c18a2c0b314cba2030e7237d63720bc84
Cosmos Nicolaou <cosmos.nicolaou@gmail.com>
2018-11-01 12:18:57 -0700

    x/jni: add missing files (#57)

---
+++ x/jni/testmain/main.go
@@ -0,0 +1,14 @@
+// Copyright 2015 The Vanadium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build java android
+
+package main
+
+import (
+	_ "v.io/x/jni/jnitest"
+)
+
+func main() {
+}
