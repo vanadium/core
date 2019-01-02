1307043975de042558be1be4609cb30f289e16e0
Cosmos Nicolaou <cnicolaou@v.io>
2019-01-02 15:02:39 -0800

    undo incorrectly removed file

---
+++ x/jni/testmain/dummy.go
@@ -0,0 +1,11 @@
+// Copyright 2015 The Vanadium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//go:generate go run v.io/x/jni/builder --output-dir=. v.io/x/jni/testmain
+
+// +build !java,!android
+
+package main
+
+func main() {}
