vanadium/core

commit 03e63fb894a07af1aa5903f43043f821a42626c4

x/ref/runtime/factories/library: allow for 'pre-parsing' of flags (#65)
Some applications (eg. reflow) would like to register the vanadium runtime flags with their own flagset and to parse those flags themselves. This revision allows for that.

x/ref/runtime/factories/library/library.go
@@ -106,8 +106,9 @@ var (
 
 type factoryState struct {
 	sync.Mutex
-	running     bool
-	initialized bool
+	running       bool
+	initialized   bool
+	rtFlagsParsed bool
 }
 
 func (st *factoryState) setRunning(s bool) {
@@ -119,10 +120,23 @@ func (st *factoryState) setRunning(s bool) {
 	st.Unlock()
 }
 
+func (st *factoryState) rtParsed() {
+	st.Lock()
+	st.rtFlagsParsed = true
+	st.Unlock()
+}
+
 func (st *factoryState) getState() (initialized, running bool) {
 	st.Lock()
 	defer st.Unlock()
 	initialized, running = st.initialized, st.running
+	return
+}
+
+func (st *factoryState) getParsingState() (runtimeParsed bool) {
+	st.Lock()
+	defer st.Unlock()
+	runtimeParsed = st.rtFlagsParsed
 	return
 }
 
@@ -133,25 +147,25 @@ func init() {
 
 // EnableCommandlineFlags enables use of command line flags.
 func EnableCommandlineFlags() {
-	EnableFlags(flag.CommandLine)
+	EnableFlags(flag.CommandLine, false)
 }
 
-// EnableFlags enables use of flags on the specified flag set.
-func EnableFlags(fs *flag.FlagSet) {
+// EnableFlags enables the use of flags on the specified flag set and returns
+// the newly created v.io/x/ref/lib/flags.Flags to allow for external
+// parsing by the caller if need be. It will optionally parse the newly
+// created and registered flags.
+func EnableFlags(fs *flag.FlagSet, parse bool) error {
 	flagSet = flags.CreateAndRegister(fs, flags.Runtime, flags.Listen, flags.Permissions)
+	if parse {
+		if err := internal.ParseFlagsIncV23Env(flagSet); err != nil {
+			return err
+		}
+		state.rtParsed()
+	}
+	return nil
 }
 
-// Init creates a new v23.Runtime.
-func Init(ctx *context.T) (v23.Runtime, *context.T, v23.Shutdown, error) {
-	initialized, running := state.getState()
-	if AllowMultipleInitializations && running {
-		return nil, nil, nil, fmt.Errorf("Library.init called whilst a previous instance is still running, the shutdown callback has not bee called")
-	}
-
-	if !AllowMultipleInitializations && initialized {
-		return nil, nil, nil, fmt.Errorf("Library.init incorrectly called multiple times")
-	}
-
+func configureLogging() error {
 	var err error
 	if ConfigureLoggingFromFlags {
 		err = logger.Manager(logger.Global()).ConfigureFromFlags(LoggingOpts...)
@@ -172,8 +186,26 @@ func Init(ctx *context.T) (v23.Runtime, *context.T, v23.Shutdown, error) {
 	}
 	if err != nil {
 		if !AllowMultipleInitializations || !logger.IsAlreadyConfiguredError(err) {
-			return nil, nil, nil, fmt.Errorf("libary.Init: %v", err)
+			return fmt.Errorf("libary.Init: %v", err)
 		}
+		return nil
+	}
+	return err
+}
+
+// Init creates a new v23.Runtime.
+func Init(ctx *context.T) (v23.Runtime, *context.T, v23.Shutdown, error) {
+	initialized, running := state.getState()
+	if AllowMultipleInitializations && running {
+		return nil, nil, nil, fmt.Errorf("Library.init called whilst a previous instance is still running, the shutdown callback has not bee called")
+	}
+
+	if !AllowMultipleInitializations && initialized {
+		return nil, nil, nil, fmt.Errorf("Library.init incorrectly called multiple times")
+	}
+
+	if err := configureLogging(); err != nil {
+		return nil, nil, nil, err
 	}
 
 	previousFlagSet := flagSet
@@ -182,9 +214,15 @@ func Init(ctx *context.T) (v23.Runtime, *context.T, v23.Shutdown, error) {
 		flagSet = flags.CreateAndRegister(dummy,
 			flags.Runtime, flags.Listen, flags.Permissions)
 	} else {
-		// Only parse flags if EnableFlags has been called.
-		if err := internal.ParseFlagsIncV23Env(flagSet); err != nil {
-			return nil, nil, nil, fmt.Errorf("library.Init: %v", err)
+		rtParsed := state.getParsingState()
+		if !rtParsed {
+			// Only parse flags if EnableFlags has been called.
+			if err := internal.ParseFlagsIncV23Env(flagSet); err != nil {
+				if err == flag.ErrHelp {
+					return nil, nil, nil, err
+				}
+				return nil, nil, nil, fmt.Errorf("library.Init: runtime flags: %v", err)
+			}
 		}
 	}
 
@@ -206,6 +244,7 @@ func Init(ctx *context.T) (v23.Runtime, *context.T, v23.Shutdown, error) {
 	}
 
 	var cancelCloud func()
+	var err error
 	if CloudVM {
 		cancelCloud, err = internal.InitCloudVM()
 		if err != nil {
@@ -236,12 +275,6 @@ func Init(ctx *context.T) (v23.Runtime, *context.T, v23.Shutdown, error) {
 		reservedDispatcher = debuglib.NewDispatcher(authorizer)
 	}
 
-	// TODO(ashankar): As of April 2016, the only purpose this non-nil
-	// publisher was serving was to enable roaming in RPC servers (see
-	// runtime/internal/flow/manager/manager.go).  Once
-	// https://vanadium-review.googlesource.com/#/c/21954/ has been merged,
-	// I will try to remove the use of the publisher from here downstream
-	// completely (and enable "roaming" for all servers by default).
 	var publisher *pubsub.Publisher
 	if Roam {
 		publisher = pubsub.NewPublisher()

v23/init.go
@@ -4,7 +4,12 @@
 
 package v23
 
-import "v.io/v23/context"
+import (
+	"flag"
+	"os"
+
+	"v.io/v23/context"
+)
 
 // TryInit is like Init, except that it returns an error instead of panicking.
 func TryInit() (*context.T, Shutdown, error) {
@@ -20,6 +25,9 @@ func TryInit() (*context.T, Shutdown, error) {
 func Init() (*context.T, Shutdown) {
 	ctx, shutdown, err := internalInit()
 	if err != nil {
+		if err == flag.ErrHelp {
+			os.Exit(1)
+		}
 		panic(err)
 	}
 	return ctx, shutdown

v23/model.go
@@ -18,6 +18,7 @@ package v23
 
 import (
 	"bytes"
+	"flag"
 	"fmt"
 	"os"
 	"runtime"
@@ -440,6 +441,9 @@ This registration is from:
 	if err != nil {
 		cancel()
 		rootcancel()
+		if err == flag.ErrHelp {
+			return nil, nil, err
+		}
 		return nil, nil, fmt.Errorf("runtimeFactory returned: %v", err)
 	}
 

x/ref/runtime/factories/library/internal/notpreparsed/main.go
@@ -1,0 +1,28 @@
+// Copyright 2017 The Vanadium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// An example using the 'library' factory which is configured via exported
+// variables rather than by command line flags.
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"v.io/v23"
+	"v.io/x/ref/runtime/factories/library"
+)
+
+func main() {
+	myfs := &flag.FlagSet{}
+	bv := false
+	myfs.BoolVar(&bv, "test", true, "set to true")
+	if err := library.EnableFlags(myfs, false); err != nil {
+		os.Exit(10)
+	}
+	_, shutdown := v23.Init()
+	defer shutdown()
+	myfs.VisitAll(func(f *flag.Flag) { fmt.Printf("%v\n", f.Name) })
+}

x/ref/runtime/factories/library/internal/preparsed/main.go
@@ -1,0 +1,28 @@
+// Copyright 2017 The Vanadium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// An example using the 'library' factory which is configured via exported
+// variables rather than by command line flags.
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"v.io/v23"
+	"v.io/x/ref/runtime/factories/library"
+)
+
+func main() {
+	myfs := &flag.FlagSet{}
+	bv := false
+	myfs.BoolVar(&bv, "test", true, "set to true")
+	if err := library.EnableFlags(myfs, true); err != nil {
+		os.Exit(10)
+	}
+	_, shutdown := v23.Init()
+	defer shutdown()
+	myfs.VisitAll(func(f *flag.Flag) { fmt.Printf("%v\n", f.Name) })
+}

x/ref/runtime/factories/library/library_test.go
@@ -123,3 +123,26 @@ func TestFlagParsing(t *testing.T) {
 		t.Errorf("got %v does not contain %v", got, want)
 	}
 }
+
+func TestPreParsing(t *testing.T) {
+	sh := gosh.NewShell(t)
+	defer sh.Cleanup()
+	sh.ContinueOnError = true
+	cmd := sh.Cmd("go",
+		"run",
+		"v.io/x/ref/runtime/factories/library/internal/preparsed",
+		"--help")
+	_, stderr := cmd.StdoutStderr()
+	if got, want := stderr, "exit status 10\n"; !strings.HasSuffix(got, want) {
+		t.Errorf("got %v does end with %v", got, want)
+	}
+	sh.Err = nil
+	cmd = sh.Cmd("go",
+		"run",
+		"v.io/x/ref/runtime/factories/library/internal/notpreparsed",
+		"--help")
+	_, stderr = cmd.StdoutStderr()
+	if got, want := stderr, "exit status 1\n"; !strings.HasSuffix(got, want) {
+		t.Errorf("got %v does end with %v", got, want)
+	}
+}

commit b5111690d4de9afabbf2245d5d189947e5cd1bb0

x/ref/runtime/factories/library: fix some typos
It seems that these typos were introduced in #65.

x/ref/runtime/factories/library/library.go
@@ -210,7 +210,7 @@ func configureLogging() error {
 	}
 	if err != nil {
 		if !AllowMultipleInitializations || !logger.IsAlreadyConfiguredError(err) {
-			return fmt.Errorf("libary.Init: %v", err)
+			return fmt.Errorf("Library.Init: %v", err)
 		}
 		return nil
 	}
@@ -244,7 +244,7 @@ func Init(ctx *context.T) (v23.Runtime, *context.T, v23.Shutdown, error) {
 				if err == flag.ErrHelp {
 					return nil, nil, nil, err
 				}
-				return nil, nil, nil, fmt.Errorf("library.Init: runtime flags: %v", err)
+				return nil, nil, nil, fmt.Errorf("Library.Init: runtime flags: %v", err)
 			}
 		}
 	}