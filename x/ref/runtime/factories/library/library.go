vanadium/core

commit 33cde07959f34c85d1c86b4ab5707abaa164aca1

v.io/ref/runtime/factories: refactor to avoid duplication (#52)
This revision refactors how factories are initialized to reduce code duplication across them. The end goal is to allow custom factories and configurations to be supportable without having to make changes to the core vanadium code.

This PR will likely break the java repository, but a new revision for that is forthcoming.

x/ref/runtime/factories/library/library.go
@@ -6,7 +6,14 @@
 
 // Package library implements a RuntimeFactory suitable for building a Vanadium
 // library that is linked into other applications. It is configured via a set
-// of exported variables rather than via command line flags.
+// of exported variables as well as optionally via the v.io/x/ref/lib/flags
+// package which allows for both internal configuration as well as via the
+// command line or environment.
+// Client packages (factories) that wish to be configured via the command line
+// must call EnableCommadLineFlags, all other users should use the
+// flags package's 'SetDefault...' methods.
+// This scheme allows this package to be broadly used to create 'factories'
+// and other initialization mechanisms whilst reducing code duplication.
 //
 // The pubsub.Publisher mechanism is used for communicating networking
 // settings to the rpc.Server implementation of the runtime and publishes
@@ -14,19 +21,18 @@
 package library
 
 import (
-	"bytes"
 	"flag"
-	"os"
+	"fmt"
+	"sync"
+	"time"
 
 	"v.io/v23"
 	"v.io/v23/context"
 	"v.io/v23/flow"
 	"v.io/v23/rpc"
-	"v.io/v23/security"
 	"v.io/v23/security/access"
 
 	"v.io/x/lib/vlog"
-	"v.io/x/ref"
 	"v.io/x/ref/internal/logger"
 	dfactory "v.io/x/ref/lib/discovery/factory"
 	"v.io/x/ref/lib/flags"
@@ -35,19 +41,11 @@ import (
 	"v.io/x/ref/runtime/internal/lib/appcycle"
 	"v.io/x/ref/runtime/internal/rt"
 	"v.io/x/ref/runtime/protocols/lib/websocket"
-	_ "v.io/x/ref/runtime/protocols/tcp"
+	_ "v.io/x/ref/runtime/protocols/tcp" // Initialize tcp, ws and wsh.
 	_ "v.io/x/ref/runtime/protocols/ws"
 	_ "v.io/x/ref/runtime/protocols/wsh"
 	"v.io/x/ref/services/debug/debuglib"
 )
-
-func defaultRoots() []string {
-	_, envRoots := ref.EnvNamespaceRoots()
-	if len(envRoots) > 0 {
-		return envRoots
-	}
-	return flags.DefaultNamespaceRoots()
-}
 
 var (
 	// LogToStderr is the equivalent of --logtostderr
@@ -69,118 +67,162 @@ var (
 	// MaxStackBufSize is the equivalent of --max_stack_buf_size
 	MaxStackBufSize vlog.MaxStackBufSize
 
-	// RuntimeFlags is the equivalent of the following flags as per
-	// v.io/x/ref/lib/flags:
-	// --v23.namespace.root (which may be repeated to supply multiple values)
-	// --v23.credentials
-	// --v23.i18n-catalogue
-	// --v23.vtrace.sample-rate
-	// --v23.vtrace.dump-on-shutdown
-	// --v23.vtrace.cache-size
-	// --v23.vtrace.collect-regexp
-	RuntimeFlags = flags.RuntimeFlags{
-		NamespaceRoots: defaultRoots(),
-		Credentials:    os.Getenv(ref.EnvCredentials),
-		I18nCatalogue:  os.Getenv(ref.EnvI18nCatalogueFiles),
-		Vtrace: flags.VtraceFlags{
-			SampleRate:     0.0,
-			DumpOnShutdown: true,
-			CacheSize:      1024,
-			LogLevel:       0,
-			CollectRegexp:  "",
-		},
-	}
+	// LoggingOpts are passed to the logging initialization functions.
+	LoggingOpts = []vlog.LoggingOpts{}
 
-	// ListenAddrs is the equivalent of the following flags as per flags.ListenFlags
-	// --v23.tcp.protocol
-	// --v23.tcp.address
-	ListenAddrs flags.ListenAddrs
-	// is the equivalent of --v23.proxy
-	ListenProxy string
-
-	// RuntimePermissionsFile is the equivalent of
-	// --v23.permissions.file=runtime:<file>
-	RuntimePermissionsFile string
-	// RuntimePermissionsLiteral is the equivalent of -v23.permissions.literal
-	RuntimePermissionsLiteral string
+	// FlagSet represents the set of flags that can be interpreted as variables.
+	// Client packages that wish to configure this package via command line
+	// flags should call EnableCommandlineFlags.
+	flagSet *flags.Flags
 
 	// Roam controls whether roaming is enabled.
-	Roam bool
+	Roam = false
+
+	// CloudVM controls whether cloud configuration is enabled.
+	CloudVM = true
+
+	// ReservedNameDispatcher controls whether a dispatcher is created
+	// for the reserved names on an RPC dispatcher. If it is set then
+	// its authorization will be determined by the PermissionsFlags and
+	// in particular the 'runtime' file therein.
+	ReservedNameDispatcher = false
+
+	// ConfigureLoggingFromFlags controls whether the logging related variables
+	// above are used for configuring logging, or if command line flags
+	// are used instead.
+	ConfigureLoggingFromFlags = false
+
+	// AllowMultipleInitializations controls whether the runtime can
+	// be initialized multiple times. The shutdown callback must be called
+	// between multiple initializations.
+	AllowMultipleInitializations = false
+
+	// ConnectionExpiryDuration sets the rate at which cached connections
+	// will be considered for eviction from the cache.
+	ConnectionExpiryDuration = 10 * time.Minute
+
+	state factoryState
 )
+
+type factoryState struct {
+	sync.Mutex
+	running     bool
+	initialized bool
+}
+
+func (st *factoryState) setRunning(s bool) {
+	st.Lock()
+	st.running = s
+	if s {
+		st.initialized = true
+	}
+	st.Unlock()
+}
+
+func (st *factoryState) getState() (initialized, running bool) {
+	st.Lock()
+	defer st.Unlock()
+	initialized, running = st.initialized, st.running
+	return
+}
 
 func init() {
 	v23.RegisterRuntimeFactory(Init)
 	flow.RegisterUnknownProtocol("wsh", websocket.WSH{})
 }
 
-func newAuthorizer() (security.Authorizer, error) {
-	if RuntimePermissionsFile == "" && RuntimePermissionsLiteral == "" {
-		return nil, nil
-	}
-	var a security.Authorizer
-	var err error
-	if RuntimePermissionsLiteral == "" {
-		a, err = access.PermissionsAuthorizerFromFile(RuntimePermissionsFile, access.TypicalTagType())
-	} else {
-		var perms access.Permissions
-		if perms, err = access.ReadPermissions(bytes.NewBufferString(RuntimePermissionsLiteral)); err == nil {
-			a = access.TypicalTagTypePermissionsAuthorizer(perms)
-		}
-	}
-	if err != nil {
-		return nil, err
-	}
-	return a, err
+// EnableCommandlineFlags enables use of command line flags.
+func EnableCommandlineFlags() {
+	flagSet = flags.CreateAndRegister(flag.CommandLine,
+		flags.Runtime, flags.Listen, flags.Permissions)
 }
 
-// AddAndParseFlags adds Vanadium-specific flags to the provided FlagSet, which
-// for this factory has no effect.
-func AddAndParseFlags(fs *flag.FlagSet) error { return nil }
-
+// Init creates a new v23.Runtime.
 func Init(ctx *context.T) (v23.Runtime, *context.T, v23.Shutdown, error) {
-	if err := logger.Manager(logger.Global()).ConfigureFromArgs(
-		LogToStderr,
-		AlsoLogToStderr,
-		LogDir,
-		Level,
-		StderrThreshold,
-		ModuleSpec,
-		FilepathSpec,
-		TraceLocation,
-		MaxStackBufSize,
-	); err != nil {
-		return nil, nil, nil, err
+	initialized, running := state.getState()
+	if AllowMultipleInitializations && running {
+		return nil, nil, nil, fmt.Errorf("Library.init called whilst a previous instance is still running, the shutdown callback has not bee called")
 	}
 
-	cancelCloud, err := internal.InitCloudVM()
+	if !AllowMultipleInitializations && initialized {
+		return nil, nil, nil, fmt.Errorf("Library.init incorrectly called multiple times")
+	}
+
+	var err error
+	if ConfigureLoggingFromFlags {
+		err = logger.Manager(logger.Global()).ConfigureFromFlags(LoggingOpts...)
+	} else {
+		opts := []vlog.LoggingOpts{
+			LogToStderr,
+			AlsoLogToStderr,
+			LogDir,
+			Level,
+			StderrThreshold,
+			ModuleSpec,
+			FilepathSpec,
+			TraceLocation,
+			MaxStackBufSize,
+		}
+		err = logger.Manager(logger.Global()).ConfigureFromArgs(
+			append(opts, LoggingOpts...)...)
+	}
 	if err != nil {
-		return nil, nil, nil, err
+		if !AllowMultipleInitializations || !logger.IsAlreadyConfiguredError(err) {
+			return nil, nil, nil, fmt.Errorf("libary.Init: %v", err)
+		}
+	}
+
+	if flagSet == nil {
+		dummy := &flag.FlagSet{}
+		flagSet = flags.CreateAndRegister(dummy,
+			flags.Runtime, flags.Listen, flags.Permissions)
+	}
+	runtimeFlags := flagSet.RuntimeFlags()
+	listenFlags := flagSet.ListenFlags()
+	permissionsFlags := flagSet.PermissionsFlags()
+
+	permissionsSpec := access.PermissionsSpec{
+		Files:   permissionsFlags.PermissionsNamesAndFiles(),
+		Literal: permissionsFlags.PermissionsLiteral(),
+	}
+
+	ishutdown := func(sf ...func()) {
+		for _, f := range sf {
+			if f != nil {
+				f()
+			}
+		}
+	}
+
+	var cancelCloud func()
+	if CloudVM {
+		cancelCloud, err = internal.InitCloudVM()
+		if err != nil {
+			return nil, nil, nil, err
+		}
 	}
 
 	ac := appcycle.New()
 	discoveryFactory, err := dfactory.New(ctx)
 	if err != nil {
-		ac.Shutdown()
-		cancelCloud()
+		ishutdown(ac.Shutdown, cancelCloud)
 		return nil, nil, nil, err
 	}
 
 	listenSpec := rpc.ListenSpec{
-		Addrs:          rpc.ListenAddrs(ListenAddrs),
-		Proxy:          ListenProxy,
+		Addrs:          rpc.ListenAddrs(listenFlags.Addrs),
+		Proxy:          listenFlags.Proxy,
 		AddressChooser: internal.NewAddressChooser(logger.Global()),
 	}
 
-	authorizer, err := newAuthorizer()
-	if err != nil {
-		return nil, nil, nil, err
-	}
-	reservedDispatcher := debuglib.NewDispatcher(authorizer)
-
-	ishutdown := func() {
-		ac.Shutdown()
-		cancelCloud()
-		discoveryFactory.Shutdown()
+	var reservedDispatcher rpc.Dispatcher
+	if ReservedNameDispatcher {
+		authorizer, err := access.AuthorizerFromSpec(
+			permissionsSpec, "runtime", access.TypicalTagType())
+		if err != nil {
+			return nil, nil, nil, err
+		}
+		reservedDispatcher = debuglib.NewDispatcher(authorizer)
 	}
 
 	// TODO(ashankar): As of April 2016, the only purpose this non-nil
@@ -194,15 +236,28 @@ func Init(ctx *context.T) (v23.Runtime, *context.T, v23.Shutdown, error) {
 		publisher = pubsub.NewPublisher()
 	}
 
-	runtime, ctx, shutdown, err := rt.Init(ctx, ac, discoveryFactory, nil, nil, &listenSpec, publisher, RuntimeFlags, reservedDispatcher, 0)
+	runtime, ctx, shutdown, err := rt.Init(ctx,
+		ac,
+		discoveryFactory,
+		nil,
+		nil,
+		&listenSpec,
+		publisher,
+		runtimeFlags,
+		reservedDispatcher,
+		permissionsSpec,
+		ConnectionExpiryDuration)
 	if err != nil {
-		ishutdown()
+		ishutdown(ac.Shutdown, cancelCloud, discoveryFactory.Shutdown)
 		return nil, nil, nil, err
 	}
 
 	runtimeFactoryShutdown := func() {
-		ishutdown()
+		ishutdown(ac.Shutdown, cancelCloud, discoveryFactory.Shutdown)
 		shutdown()
+		flagSet = nil
+		state.setRunning(false)
 	}
+	state.setRunning(true)
 	return runtime, ctx, runtimeFactoryShutdown, nil
 }

.travis.yml
@@ -1,4 +1,4 @@
-sudo: required
+sudo: false
 dist: trusty
 language: go
 go:
@@ -8,20 +8,20 @@ env:
   matrix:
   - VDLPATH=$GOPATH/src
   global:
-  - secure: E5fEWzKMEJSnCB0cqVrONV67bzHcbNPkIx3MmHTf1ARmBSMiNfa6km4nNxPcmTYXf0JyVyTdivsvL116/3K7cRUwAV+zwx35lo24/braJHrtt32XdPYOy7D2PmQbnV4xiwUiMzy1s9t7wpEVmqnfKJqqfv0qzz8JfqVkH9iIxVPR8pvSW3gXKcpb7Vh2JIGFEYoMr1JOqFZ7W0XH61wyJq3TAJxJZ8n4AV9Rrj20GGL6QNdEkafaUpBZotb3fi9naFreApDdF9ET8MZAYz49mGRlFJa5pNd782yrd1rV08Q2WU3SVHWkm/Ij6hKLBWQ+pXYPC2miKhEYK2dVHQfQA8DJy+9FFuvVMB6hcM1TkRo59EaK/E+PXGnTtdW3L9QpneajYSpirNJEevBnmHPBfZTRNvSFgS5qwtcxPoo3uNnBCZ3jF1dRWzUd2AoBikqOEXMn1cgpVi6BilYZ4GZ85qyBO70jNrG4nNtwHC8SvYuJSEURwUkjRVuxAl0upBo1pTrBbdrW+BOR+W0xuyxX0PLWwxGoMDtiwEQHdWC7CAjc3gH22F0CTB84VvK/VMzqJD7t5pauw8yi/CK5KiG7xBk9ueV/MZyfILPSyDbjM10BrsEHF+7QajF/Di7gfWbwS1h/iiAWU1j8wJa0B+E5MEwhV45Si0ww10ByyPm71A0=
+  - secure: tQWVg6I35tFHQ8fBGMFDaAYcGVT0jfbakdprDOBRNELNz/H2/lPgiX3MIfzNZOFFf5We8ZLaDrn/aoccrWAYfJPoyLnAcnvDTCA95HldJBt37NTHMGtA8QwDTyFCviYKLt9liDJUeNNE2qpJdUNiXWUG9Arqa0sWXA/enAFFAf8Rs/823htMclJTnZ268ASoGiBs5L59mvfeWuTN96eVKFTFuQroPywX1SUImdbx8CeH2eXs7GnnrepzO5jlPeneA10OKiCt26zE7RtZbI0PfhO+douB8lhC7SCe6/et+5vE6UHsyXvnjhr2+MQQc/SFRfOp5X2kYjx7Y/XG4nn7bGD6P3rDicmvs5X5L4iZK/7OSe+PI5/ZNVW6eeKQIQqjLg3kOZnCA58k38LRG9d5xE475ZS/8NguOiVKP5kP852ldNRJoHogF2wTMQUGt6iJlSDJIc+yHIWZ0njsxXjJ1zu3bq4FYrLfnORgn2kqW0lFRE+7W5QEhaIlknacMTM6Lm3DRG59IV3/Vq5E3D4O9j2NbPxHJhqu+RfKNZkWVmMHy4GIwtCxl0ZqiTZANwcITNMnlGeunlHcMHyHCG6r6ukNmUaSbSQJJGZYtksffqoKykgCN/bvjuTiDkusI0CiSQA3lwntUcfCv+WefugCOKJ3PwF5RQoGZenfpvewIWU=
 install:
 - echo "GOPATH:" $GOPATH
 - echo "VDLPATH:" $VDLPATH
-- go get -v -t v.io/...
+- go get -t v.io/...
 - go install v.io/...
 script:
-- make test
+- go test v.io/...
 - make test-integration
 notifications:
   email:
     receipients:
-      - vanadium-ci@v.io
+    - vanadium-ci@v.io
     on_success: never
     on_failure: always
 after_success:
-- bash ./trigger-travis-build.sh $TRAVIS_API_TOKEN vanadium java
+- bash ./trigger-travis-build.sh "$TRAVIS_API_TOKEN" vanadium java

Makefile
@@ -6,26 +6,9 @@ export GOPATH
 VDLPATH ?= $(shell pwd)/src
 export VDLPATH
 
-# Note that the split across the core and go.lib repos leads to
-# a multi-tree layout when developing for contributing code rather than
-# the single tree layout used when 'using' the code via go get for example.
-# The VANADIUM_CORE_REPO variable is used to unambigously refer to the
-# core repo.
-VANADIUM_CORE_REPO ?= $(shell pwd)
-export VANADIUM_CORE_REPO
-
-.PHOHY: go
-go:
-	go install v.io/...
-
-.PHONY: test
-test:
-	echo "GOPATH" ${GOPATH}
-	echo "VDLPATH" ${VDLPATH}
-	go test v.io/...
-
 .PHONY: test-integration
 test-integration:
+	@echo "GOPATH" ${GOPATH}
 	@echo "VDLPATH" ${VDLPATH}
 	go test \
 		v.io/x/ref/cmd/principal \
@@ -41,15 +24,3 @@ test-integration:
 		v.io/x/ref/examples/rps/rpsbot \
 		-v23.tests
 
-.PHONY: java
-java: go
-	@echo "VANADIUM_CORE_REPO" ${VANADIUM_CORE_REPO}
-	cd java/lib && ../gradlew -i publishToMavenLocal
-
-.PHONY: test-java
-test-java: java
-	cd java/lib && ../gradlew -i test
-
-.PHONY: clean
-clean:
-	rm -rf go/bin go/pkg

trigger-travis-build.sh
@@ -1,12 +1,28 @@
+#!/bin/bash
+
+token="$1"
+owner="$2"
+repo="$3"
+
+if [[ -z "$token" ]]; then
+    echo "no token specified"
+    exit 1
+if
+if [[ -z "$owner" ]]; then
+    echo "no owner specified"
+    exit 1
+if
+if [[ -z "$repo" ]]; then
+    echo "no repo specified"
+    exit 1
+if
+
 body='{
 "request": {
 "branch":"master"
 }}'
 
-token="$1"
-owner="$2"
-repo="$3"
-curl -v -s -X POST \
+curl -s -X POST \
    -H "Content-Type: application/json" \
    -H "Accept: application/json" \
    -H "Travis-API-Version: 3" \

v23/init_internal_test.go
@@ -15,6 +15,7 @@ import (
 	"v.io/v23/namespace"
 	"v.io/v23/rpc"
 	"v.io/v23/security"
+	"v.io/v23/security/access"
 )
 
 // Create a mock RuntimeFactory.
@@ -36,9 +37,13 @@ func (*mockRuntime) GetClient(ctx *context.T) rpc.Client { return nil }
 func (*mockRuntime) WithNewNamespace(ctx *context.T, roots ...string) (*context.T, namespace.T, error) {
 	return nil, nil, nil
 }
-func (*mockRuntime) GetNamespace(ctx *context.T) namespace.T         { return nil }
-func (*mockRuntime) GetAppCycle(ctx *context.T) AppCycle             { return nil }
-func (*mockRuntime) GetListenSpec(ctx *context.T) rpc.ListenSpec     { return rpc.ListenSpec{} }
+func (*mockRuntime) GetNamespace(ctx *context.T) namespace.T     { return nil }
+func (*mockRuntime) GetAppCycle(ctx *context.T) AppCycle         { return nil }
+func (*mockRuntime) GetListenSpec(ctx *context.T) rpc.ListenSpec { return rpc.ListenSpec{} }
+
+func (*mockRuntime) GetPermissionsSpec(ctx *context.T) access.PermissionsSpec {
+	return access.PermissionsSpec{}
+}
 func (*mockRuntime) WithBackgroundContext(ctx *context.T) *context.T { return nil }
 func (*mockRuntime) GetBackgroundContext(ctx *context.T) *context.T  { return nil }
 

v23/model.go
@@ -19,6 +19,7 @@ package v23
 import (
 	"bytes"
 	"fmt"
+	"os"
 	"runtime"
 	"sync"
 	"time"
@@ -29,6 +30,7 @@ import (
 	"v.io/v23/namespace"
 	"v.io/v23/rpc"
 	"v.io/v23/security"
+	"v.io/v23/security/access"
 )
 
 const (
@@ -139,6 +141,9 @@ type Runtime interface {
 	// WithListenSpec attaches a ListenSpec to the returned context.
 	WithListenSpec(ctx *context.T, ls rpc.ListenSpec) *context.T
 
+	// GetPermissionSpec returns the PermissonsSpec stored in 'ctx'.
+	GetPermissionsSpec(ctx *context.T) access.PermissionsSpec
+
 	// WithBackgroundContext creates a new context derived from 'ctx'
 	// with the given context set as the background context.
 	WithBackgroundContext(ctx *context.T) *context.T
@@ -248,10 +253,9 @@ func WithListenSpec(ctx *context.T, ls rpc.ListenSpec) *context.T {
 	return initState.currentRuntime().WithListenSpec(ctx, ls)
 }
 
-// WithBackgroundContext creates a new context derived from 'ctx'
-// with the given context set as the background context.
-func WithBackgroundContext(ctx *context.T) *context.T {
-	return initState.runtime.WithBackgroundContext(ctx)
+// GetPermissionsSpec returns the Permissions for the specified name in 'ctx'.
+func GetPermissionsSpec(ctx *context.T) access.PermissionsSpec {
+	return initState.currentRuntime().GetPermissionsSpec(ctx)
 }
 
 // GetBackgroundContext returns a background context. This context can be used
@@ -436,7 +440,7 @@ This registration is from:
 	if err != nil {
 		cancel()
 		rootcancel()
-		return nil, nil, err
+		return nil, nil, fmt.Errorf("runtimeFactory returned: %v", err)
 	}
 
 	initState.mu.Lock()
@@ -444,6 +448,10 @@ This registration is from:
 	initState.mu.Unlock()
 
 	vshutdown := func() {
+		if r := recover(); r != nil {
+			fmt.Fprintf(os.Stderr, "WARNING: panic called: bypassing shutdown code\n")
+			panic(r)
+		}
 		// Note we call our own cancel here to ensure that the
 		// runtime/runtimeFactory implementor has not attached anything to a
 		// non-cancellable context.
@@ -459,7 +467,7 @@ This registration is from:
 
 	if err := rt.Init(ctx); err != nil {
 		vshutdown()
-		return nil, nil, err
+		return nil, nil, fmt.Errorf("rt.Init returned: %v", err)
 	}
 
 	return ctx, vshutdown, nil

v23/security/access/authorizer.go
@@ -5,6 +5,8 @@
 package access
 
 import (
+	"bytes"
+	"fmt"
 	"os"
 
 	"v.io/v23/context"
@@ -108,6 +110,48 @@ func PermissionsAuthorizerFromFile(filename string, tagType *vdl.Type) (security
 		return nil, errTagType(tagType)
 	}
 	return &fileAuthorizer{filename, tagType}, nil
+}
+
+// PermissionsSpec represents a specification for permissions derived
+// from command line flags or some other means.
+type PermissionsSpec struct {
+	// Files represents a set of named files that contain permissions.
+	// The name 'runtime' is reserved for use by the runtime.
+	Files map[string]string
+	// Literal represents a literal, ie. json, permissions specification.
+	Literal string
+}
+
+// AuthorizerFromSpec creates an authorizer as specified by the
+// supplied specification. If no permissions are specified then the
+// default, (ie. nil) Authorizer is returned.
+func AuthorizerFromSpec(ps PermissionsSpec, name string, tagType *vdl.Type) (security.Authorizer, error) {
+	if len(ps.Literal) == 0 {
+		filename := ps.Files[name]
+		if len(filename) == 0 {
+			return nil, nil
+		}
+		return PermissionsAuthorizerFromFile(ps.Files[name], tagType)
+	}
+	perms, err := ReadPermissions(bytes.NewBufferString(ps.Literal))
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse permissions literal: %v: %v", ps.Literal, err)
+	}
+	return PermissionsAuthorizer(perms, tagType)
+}
+
+// RuntimeAuthorizer returns an authorizer configured in the same way
+// as that for 'runtime', preferring a literal (ie. on the command line)
+// vs a file based specification, and assuming the typical set of access tags.
+func RuntimeAuthorizer(spec PermissionsSpec) security.Authorizer {
+	return TypicalTagAuthorizerFromSpec(spec, "runtime")
+}
+
+// TypicalTagAuthorizerFromSpec assumes TypicalTagType() and thus avoids
+// returning an error.
+func TypicalTagAuthorizerFromSpec(ps PermissionsSpec, name string) security.Authorizer {
+	a, _ := AuthorizerFromSpec(ps, name, TypicalTagType())
+	return a
 }
 
 func errTagType(tt *vdl.Type) error {

x/jni/builder/java_builder.go
@@ -1,0 +1,66 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"runtime"
+
+	"v.io/x/lib/gosh"
+)
+
+type tb struct{}
+
+func (t *tb) FailNow() {
+	log.Panicf("build failed")
+}
+func (t *tb) Logf(format string, args ...interface{}) {
+	log.Printf(format, args...)
+}
+
+var (
+	packageName   string
+	outputDirName string
+	outputLibName string
+)
+
+func init() {
+	flag.StringVar(&packageName, "package-name", "v.io/x/jni/main", "build the specified package as a .so/.dylib for use with java")
+	flag.StringVar(&outputDirName, "output-dir", "", "the directory for the output file")
+	flag.StringVar(&outputLibName, "output-lib", "libv23", "the prefix for the output file, .so or .dylib will be appended appropriately")
+}
+
+func main() {
+	flag.Parse()
+
+	javaHome := os.Getenv("JAVA_HOME")
+	if len(javaHome) == 0 {
+		log.Fatalf("JAVA_HOME not specified")
+	}
+	if len(outputDirName) == 0 {
+		log.Fatalf("--output-dir not specified")
+	}
+
+	if runtime.GOOS == "darwin" {
+		outputLibName += ".dylib"
+	} else {
+		outputLibName += ".so"
+	}
+
+	sh := gosh.NewShell(&tb{})
+	sh.PropagateChildOutput = true
+	defer sh.Cleanup()
+
+	cmd := sh.Cmd(
+		"go",
+		"build",
+		"-buildmode=c-shared",
+		"-v",
+		"-tags", "java cgo",
+		"-o", filepath.Join(outputDirName, outputLibName),
+		packageName)
+	cmd.Vars["CGO_CFLAGS"] = fmt.Sprintf("-I%v/include -I%v/include/%v", javaHome, javaHome, runtime.GOOS)
+	cmd.Run()
+}

commit 33694c82ac4e79a894e2c95ee795d542c4582a01

x/ref/runtime/factories/library: fix some typos (#115)
x/ref/runtime/factories/library: fix some typos

It seems that these typos were introduced in #52.

x/ref/runtime/factories/library/library.go
@@ -210,7 +210,7 @@ func configureLogging() error {
 	}
 	if err != nil {
 		if !AllowMultipleInitializations || !logger.IsAlreadyConfiguredError(err) {
-			return fmt.Errorf("libary.Init: %v", err)
+			return fmt.Errorf("library.Init: %v", err)
 		}
 		return nil
 	}
@@ -221,11 +221,11 @@ func configureLogging() error {
 func Init(ctx *context.T) (v23.Runtime, *context.T, v23.Shutdown, error) {
 	initialized, running := state.getState()
 	if AllowMultipleInitializations && running {
-		return nil, nil, nil, fmt.Errorf("Library.init called whilst a previous instance is still running, the shutdown callback has not bee called")
+		return nil, nil, nil, fmt.Errorf("library.Init called whilst a previous instance is still running, the shutdown callback has not bee called")
 	}
 
 	if !AllowMultipleInitializations && initialized {
-		return nil, nil, nil, fmt.Errorf("Library.init incorrectly called multiple times")
+		return nil, nil, nil, fmt.Errorf("library.Init incorrectly called multiple times")
 	}
 
 	if err := configureLogging(); err != nil {
@@ -244,7 +244,7 @@ func Init(ctx *context.T) (v23.Runtime, *context.T, v23.Shutdown, error) {
 				if err == flag.ErrHelp {
 					return nil, nil, nil, err
 				}
-				return nil, nil, nil, fmt.Errorf("library.Init: runtime flags: %v", err)
+				return nil, nil, nil, fmt.Errorf("library.Init: runtime flags: %w", err)
 			}
 		}
 	}