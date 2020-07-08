9658b530c9a3a1e8c03704bcb55e9c62078a35b4
Cosmos Nicolaou <cnicolaou@cloudeng.io>
2020-07-08 11:04:13 -0700

    lint

--- v23/security/ed25519_go.go
+++ v23/security/ed25519_go.go
@@ -9,14 +9,9 @@ package security
 import "crypto/ed25519"
 
 func newInMemoryED25519SignerImpl(key ed25519.PrivateKey) (Signer, error) {
-	sign := func(data []byte) ([]byte, error) {
-		return ed25519.Sign(key, data), nil
-	}
-	pk := key.Public()
-	ed25519PK := pk.(ed25519.PublicKey)
-	return &ed25519Signer{sign: sign, pubkey: &ed25519PublicKey{ed25519PK}}, nil
+	return newGoStdlibED25519Signer(key)
 }
 
 func newED25519PublicKeyImpl(key ed25519.PublicKey) PublicKey {
-	return &ed25519PublicKey{key}
+	return newGoStdlibED25519PublicKey(key)
 }
