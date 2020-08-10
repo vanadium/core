fb26a0a9cd3c763b50e02c4e61421fe640b9ade0
Cosmos Nicolaou <cnicolaou@cloudeng.io>
2020-08-10 09:14:42 -0700

    update comment

--- x/ref/envvar.go
+++ x/ref/envvar.go
@@ -42,6 +42,7 @@ const (
 
 	// When EnvExpectGoogleComputeEngine is set and non-empty, the runtime
 	// initialization will fail if the GCE metadata is inaccessible.
+	// DEPRECATED: use V23_VIRTUALIZATION_PROVIDER instead.
 	EnvExpectGoogleComputeEngine = "V23_EXPECT_GOOGLE_COMPUTE_ENGINE"
 
 	// EnvVirtualizationProvider is the name of the environment variable
