66c5ca5744b9412e0216b352d840b9a61170da61
Noah Tseng <ntseng@grailbio.com>
2020-09-21 14:45:17 -0700

    x/ref/runtime/internal/rpc,v23/requestid: add requestid to flowserver ctx

---
+++ x/ref/runtime/internal/requestid/model.go
@@ -0,0 +1,30 @@
+package requestid
+
+import (
+	"github.com/google/uuid"
+	"v.io/v23/context"
+)
+
+type contextKey int
+
+const requestIDKey = contextKey(iota)
+
+func WithNewRequestIDLoggingPrefix(ctx *context.T) *context.T {
+	requestID, err := uuid.NewUUID()
+	if err != nil {
+		panic(err)
+	}
+	return  context.WithLoggingPrefix(ctx, requestID)
+}
+func WithNewRequestID(ctx *context.T) *context.T {
+	requestID, err := uuid.NewUUID()
+	if err != nil {
+		panic(err)
+	}
+	return  context.WithValue(ctx, requestIDKey, requestID)
+}
+
+func RequestID(ctx *context.T) uuid.UUID {
+	requestID := ctx.Value(requestIDKey).(uuid.UUID)
+	return requestID
+}
