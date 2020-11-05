7361d5b8125e910cdfba9449dc5ec25766a71d71
noahtseng <noahtseng@berkeley.edu>
2020-11-04 16:02:07 -0800

    Request id public (#168)
    * v23/requestid: refactor requestid from x/ref/runtime/internal/ to v23/
    
    Co-authored-by: Noah Tseng <ntseng@grailbio.com>

--- x/ref/runtime/internal/rpc/server.go
+++ x/ref/runtime/internal/rpc/server.go
@@ -20,6 +20,7 @@ import (
 	"v.io/v23/i18n"
 	"v.io/v23/naming"
 	"v.io/v23/options"
+	"v.io/v23/requestid"
 	"v.io/v23/rpc"
 	"v.io/v23/security"
 	"v.io/v23/security/access"
@@ -32,7 +33,6 @@ import (
 	"v.io/x/ref/lib/stats"
 	"v.io/x/ref/runtime/internal/flow/conn"
 	"v.io/x/ref/runtime/internal/flow/manager"
-	"v.io/x/ref/runtime/internal/requestid"
 )
 
 const (
