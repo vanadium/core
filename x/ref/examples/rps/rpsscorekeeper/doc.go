5efd53a051baf96a1818ad6c88435fcf1193bb8d
Cosmos Nicolaou <cnicolaou@cloudeng.io>
2021-05-05 18:22:05 -0700

    .

--- x/ref/examples/rps/rpsscorekeeper/doc.go
+++ x/ref/examples/rps/rpsscorekeeper/doc.go
@@ -86,6 +86,8 @@ The global flags are:
    collection
  -v23.vtrace.dump-on-shutdown=true
    If true, dump all stored traces on runtime shutdown
+ -v23.vtrace.enable-aws-xray=false
+   Enable the use of AWS x-ray integration with vtrace
  -v23.vtrace.sample-rate=0
    Rate (from 0.0 to 1.0) to sample vtrace traces
  -v23.vtrace.v=0
