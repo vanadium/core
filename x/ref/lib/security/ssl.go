c0b24af61ca048c7be2e85c61827005387162b2e
Cosmos Nicolaou <cnicolaou@cloudeng.io>
2021-09-16 21:53:07 -0700

    .

---
+++ x/ref/lib/security/ssl.go
@@ -0,0 +1,119 @@
+// Copyright 2021 The Vanadium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package security
+
+import (
+	"crypto"
+	"crypto/ecdsa"
+	"crypto/ed25519"
+	"crypto/rsa"
+	"crypto/x509"
+	"fmt"
+	"io"
+	"os"
+	"time"
+
+	"v.io/v23/security"
+	"v.io/x/ref/lib/security/internal"
+)
+
+// ParsePEMPrivateKeyFile parses an pem format private key from the specified file.
+func ParsePEMPrivateKeyFile(keyFile string, passphrase []byte) (crypto.PrivateKey, error) {
+	f, err := os.Open(keyFile)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	key, err := internal.LoadPEMPrivateKey(f, passphrase)
+	if err != nil {
+		return nil, fmt.Errorf("failed to load: %v: %v", keyFile, err)
+	}
+	return key, nil
+}
+
+// X509CertificateInfo represents a subset of the information in an x509
+// certificate.
+type X509CertificateInfo struct {
+	PublicKey           security.PublicKey
+	NotBefore, NotAfter time.Time
+	Issuer, Subject     string
+}
+
+// ParseOpenSSLCertificateFile parses an ssl/tls public key from the specified file.
+func ParseOpenSSLCertificateFile(filename string, verifyOpts x509.VerifyOptions) (X509CertificateInfo, error) {
+	f, err := os.Open(filename)
+	if err != nil {
+		return X509CertificateInfo{}, err
+	}
+	defer f.Close()
+	return ParseOpenSSLCertificate(f, verifyOpts)
+}
+
+// ParseOpenSSLCertificate parses an ssl/tls public key from the specified io.Reader.
+func ParseOpenSSLCertificate(rd io.Reader, verifyOpts x509.VerifyOptions) (X509CertificateInfo, error) {
+	c, err := internal.LoadCertificate(rd)
+	if err != nil {
+		return X509CertificateInfo{}, fmt.Errorf("failed to parse x509.Certificate: %v", err)
+	}
+	chains, err := c.Verify(verifyOpts)
+	if err != nil {
+		return X509CertificateInfo{}, fmt.Errorf("failed to verify x509.Certificate: %v", err)
+	}
+	if len(chains) == 0 || len(chains[0]) == 0 {
+		return X509CertificateInfo{}, fmt.Errorf("no verified chains in x509.Certificate")
+	}
+	cert := chains[0][0]
+	if len(cert.Issuer.CommonName) == 0 {
+		return X509CertificateInfo{}, fmt.Errorf("x509 certificate Issuer has no Common Name")
+	}
+	if len(cert.Subject.CommonName) == 0 {
+		return X509CertificateInfo{}, fmt.Errorf("x509 certificate Subject has no Common Name")
+	}
+	if cert.PublicKey == nil {
+		return X509CertificateInfo{}, fmt.Errorf("x509 certificate has no public key")
+	}
+
+	pk, err := NewPublicKey(cert.PublicKey)
+	if err != nil {
+		return X509CertificateInfo{}, fmt.Errorf("x509 public key type is not supported: %v", err)
+	}
+
+	return X509CertificateInfo{
+		PublicKey: pk,
+		NotBefore: cert.NotBefore,
+		NotAfter:  cert.NotAfter,
+		Issuer:    cert.Issuer.CommonName,
+		Subject:   cert.Subject.CommonName,
+	}, nil
+}
+
+// NewInMemorySigner creates a new security.Signer that stores its
+// private key in memory using the security.NewInMemory{ECDSA,ED25519,RSA}Signer
+// methods.
+func NewInMemorySigner(key crypto.PrivateKey) (security.Signer, error) {
+	switch k := key.(type) {
+	case *ecdsa.PrivateKey:
+		return security.NewInMemoryECDSASigner(k)
+	case *rsa.PrivateKey:
+		return security.NewInMemoryRSASigner(k)
+	case ed25519.PrivateKey:
+		return security.NewInMemoryED25519Signer(k)
+	}
+	return nil, fmt.Errorf("%T is an unsupported key type", key)
+}
+
+// NewPublicKey creates a new security.PublicKey for the supplied
+// crypto.PublicKey.
+func NewPublicKey(key crypto.PublicKey) (security.PublicKey, error) {
+	switch k := key.(type) {
+	case *ecdsa.PublicKey:
+		return security.NewECDSAPublicKey(k), nil
+	case *rsa.PublicKey:
+		return security.NewRSAPublicKey(k), nil
+	case ed25519.PublicKey:
+		return security.NewED25519PublicKey(k), nil
+	}
+	return nil, fmt.Errorf("%T is an unsupported key type", key)
+}
---
+++ x/ref/lib/security/ssl_test.go
@@ -0,0 +1,118 @@
+// Copyright 2021 The Vanadium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package security_test
+
+import (
+	"crypto/x509"
+	"os"
+	"path/filepath"
+	"testing"
+
+	seclib "v.io/x/ref/lib/security"
+	"v.io/x/ref/lib/security/internal"
+)
+
+func customCertPool(t *testing.T, cafile string) *x509.CertPool {
+	rf, err := os.Open(cafile)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer rf.Close()
+	rootCert, err := internal.LoadCertificate(rf)
+	if err != nil {
+		t.Fatal(err)
+	}
+	certPool := x509.NewCertPool()
+	certPool.AddCert(rootCert)
+	return certPool
+}
+
+func TestSSLKeys(t *testing.T) {
+	certPool := customCertPool(t, filepath.Join("testdata", "root-ca.pem"))
+	purpose, message := []byte("testing"), []byte("a message")
+	for _, tc := range []struct {
+		prefix string
+		pk     string
+	}{
+		{"ec256.vanadium.io", "7e:1f:4d:6d:99:3b:6c:51:16:90:83:cf:07:a9:a3:fc"},
+		{"ed25519.vanadium.io", "c4:33:17:69:02:42:8c:19:5d:69:77:02:71:c5:1d:7e"},
+		{"rsa2048.vanadium.io", "53:fb:b2:07:10:fd:9c:89:16:f5:76:4b:e8:5c:17:30"},
+		{"rsa4096.vanadium.io", "10:d6:7b:2f:7d:a2:6b:96:c1:27:50:05:ce:d6:d5:26"},
+	} {
+
+		privKeyFile := tc.prefix + ".key"
+		cpriv, err := seclib.ParsePEMPrivateKeyFile(filepath.Join("testdata", privKeyFile),
+			nil)
+		if err != nil {
+			t.Errorf("failed to load %v: %v", privKeyFile, err)
+		}
+		signer, err := seclib.NewInMemorySigner(cpriv)
+		if err != nil {
+			t.Errorf("failed to create signer for %v: %v", privKeyFile, err)
+		}
+		sig, err := signer.Sign(purpose, message)
+		if err != nil {
+			t.Errorf("failed to sign using %v: %v", privKeyFile, err)
+		}
+		if !sig.Verify(signer.PublicKey(), message) {
+			t.Errorf("failed to verify signature using %v: %v", privKeyFile, err)
+		}
+
+		opts := x509.VerifyOptions{
+			Roots: certPool,
+		}
+		crtFile := tc.prefix + ".crt"
+		cert, err := seclib.ParseOpenSSLCertificateFile(filepath.Join("testdata", crtFile), opts)
+		if err != nil {
+			t.Errorf("failed to load %v: %v", crtFile, err)
+			continue
+		}
+
+		if got, want := cert.PublicKey.String(), tc.pk; got != want {
+			t.Errorf("%v: got %v, want %v", crtFile, got, want)
+		}
+	}
+}
+
+func TestLetsEncryptKeys(t *testing.T) {
+	filename := filepath.Join("testdata", "www.labdrive.io.letsencrypt")
+	rf, err := os.Open(filename)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer rf.Close()
+
+	purpose, message := []byte("testing"), []byte("another message")
+
+	cpriv, err := seclib.ParsePEMPrivateKeyFile(filename, nil)
+	if err != nil {
+		t.Errorf("failed to load %v: %v", filename, err)
+	}
+	signer, err := seclib.NewInMemorySigner(cpriv)
+	if err != nil {
+		t.Errorf("failed to create signer for %v: %v", filename, err)
+	}
+	sig, err := signer.Sign(purpose, message)
+	if err != nil {
+		t.Errorf("failed to sign using %v: %v", filename, err)
+	}
+	if !sig.Verify(signer.PublicKey(), message) {
+		t.Errorf("failed to verify signature using %v: %v", filename, err)
+	}
+	certPool := customCertPool(t, filepath.Join("testdata", "letsencrypt-stg-int-e1.pem"))
+	opts := x509.VerifyOptions{
+		Roots: certPool,
+	}
+	cert, err := seclib.ParseOpenSSLCertificateFile(filename, opts)
+	if err != nil {
+		t.Fatalf("failed to load %v: %v", filename, err)
+	}
+
+	// openssl x509 -in testdata/letsencrypt-stg-int-e1.pem --pubkey --noout
+	// openssl ec -in x.pem --pubin --inform PEM --outform DER --noout |openssl md5 -c
+	if got, want := cert.PublicKey.String(), "d4:1d:8c:d9:8f:00:b2:04:e9:80:09:98:ec:f8:42:7e"; got != want {
+		t.Errorf("%v: got %v, want %v", filename, got, want)
+	}
+}
