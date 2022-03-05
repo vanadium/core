354506aa62f96994490a749bcf4766eb12a6fa02
Cosmos Nicolaou <cnicolaou@cloudeng.io>
2022-03-05 13:09:55 -0800

    .

--- v23/security/openssl_util_test.go
+++ v23/security/openssl_util_test.go
@@ -13,16 +13,6 @@ import (
 	"crypto/rsa"
 )
 
-func NewOpenSSLECDSASigner(k *ecdsa.PrivateKey) (Signer, error) {
-	return newOpenSSLECDSASigner(k, ecdsaHash(&k.PublicKey))
-}
-func NewOpenSSLED25519Signer(k ed25519.PrivateKey) (Signer, error) {
-	return newOpenSSLED25519Signer(k, SHA512Hash)
-}
-func NewOpenSSLRSASigner(k *rsa.PrivateKey) (Signer, error) {
-	return newOpenSSLRSASigner(k, SHA512Hash)
-}
-
 var (
 	NewGoStdlibECDSASigner   = newGoStdlibECDSASigner
 	NewGoStdlibED25519Signer = newGoStdlibED25519Signer
@@ -39,18 +29,6 @@ func NewOpenSSLED25519Signer(key ed25519.PrivateKey) (Signer, error) {
 
 func NewOpenSSLRSASigner(key *rsa.PrivateKey) (Signer, error) {
 	return newOpenSSLRSASigner(key, SHA512Hash)
-}
-
-func NewGoStdlibECDSASigner(key *ecdsa.PrivateKey) (Signer, error) {
-	return newGoStdlibECDSASigner(key, ecdsaHash(&key.PublicKey))
-}
-
-func NewGoStdlibED25519Signer(key ed25519.PrivateKey) (Signer, error) {
-	return newGoStdlibED25519Signer(key, SHA512Hash)
-}
-
-func NewGoStdlibRSASigner(key *rsa.PrivateKey) (Signer, error) {
-	return newGoStdlibRSASigner(key, SHA512Hash)
 }
 
 func OpenSSLVersion() string {
