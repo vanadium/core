9bd859002b888745ef92e5bf1e0e30522a1f52e3
Cosmos Nicolaou <cnicolaou@cloudeng.io>
2022-09-02 10:36:19 -0700

    .

--- x/ref/runtime/protocols/lib/framer/framer_test.go
+++ x/ref/runtime/protocols/lib/framer/framer_test.go
@@ -19,53 +19,52 @@ func (*readWriteCloser) Close() error {
 
 func TestFramer(t *testing.T) {
 	f := &framer{ReadWriteCloser: &readWriteCloser{}}
+
+	writeAndRead := func(want []byte, bufs [][]byte) {
+		l := len(want)
+		if n, err := f.WriteMsg(bufs...); err != nil || n != l {
+			t.Fatalf("got %v, %v, want %v, nil", n, err, l)
+		}
+		if got, err := f.ReadMsg(); err != nil || !bytes.Equal(got, want) {
+			t.Errorf("got %v, %v, want %v, nil", got, err, want)
+		}
+
+		if n, err := f.WriteMsg(bufs...); err != nil || n != l {
+			t.Fatalf("got %v, %v, want %v, nil", n, err, l)
+		}
+
+		rbuf := make([]byte, len(want)*2)
+		got, err := f.ReadMsg2(rbuf)
+		if err != nil || !bytes.Equal(got, want) {
+			t.Errorf("got %v, %v, want %v, nil", got, err, want)
+		}
+		rbuf[0] = 0xff
+		if bytes.Equal(got, want) {
+			t.Errorf("looks like ReadMsg2 did not use the supplied buffer")
+		}
+	}
+
 	bufs := [][]byte{[]byte("read "), []byte("this "), []byte("please.")}
 	want := []byte("read this please.")
-	l := len(want)
-	if n, err := f.WriteMsg(bufs...); err != nil || n != l {
-		t.Fatalf("got %v, %v, want %v, nil", n, err, l)
-	}
-	if got, err := f.ReadMsg(); err != nil || !bytes.Equal(got, want) {
-		t.Errorf("got %v, %v, want %v, nil", got, err, want)
-	}
+
+	writeAndRead(want, bufs)
+
 	// Framing a smaller message afterwards should reuse the internal buffer
 	// from the first sent message.
+	oldBufferLen := len(want) + 3
 	bufs = [][]byte{[]byte("read "), []byte("this "), []byte("too.")}
 	want = []byte("read this too.")
-	oldBufferLen := l + 3
-	l = len(want)
-	if n, err := f.WriteMsg(bufs...); err != nil || n != l {
-		t.Fatalf("got %v, %v, want %v, nil", n, err, l)
-	}
-	if got, err := f.ReadMsg(); err != nil || !bytes.Equal(got, want) {
-		t.Errorf("got %v, %v, want %v, nil", got, err, want)
-	}
+
+	writeAndRead(want, bufs)
+
 	if len(f.buf) != oldBufferLen {
 		t.Errorf("framer internal buffer should have been reused")
 	}
 	// Sending larger message afterwards should work as well.
 	bufs = [][]byte{[]byte("read "), []byte("this "), []byte("way bigger message.")}
 	want = []byte("read this way bigger message.")
-	l = len(want)
-	if n, err := f.WriteMsg(bufs...); err != nil || n != l {
-		t.Fatalf("got %v, %v, want %v, nil", n, err, l)
-	}
-	if got, err := f.ReadMsg(); err != nil || !bytes.Equal(got, want) {
-		t.Errorf("got %v, %v, want %v, nil", got, err, want)
-	}
 
-	if n, err := f.WriteMsg(bufs...); err != nil || n != l {
-		t.Fatalf("got %v, %v, want %v, nil", n, err, l)
-	}
-	rbuf := make([]byte, len(want)*2)
-	got, err := f.ReadMsg2(rbuf)
-	if err != nil || !bytes.Equal(got, want) {
-		t.Errorf("got %v, %v, want %v, nil", got, err, want)
-	}
-	rbuf[0] = 0xff
-	if bytes.Equal(got, want) {
-		t.Errorf("looks like ReadMsg2 did not use the supplied buffer")
-	}
+	writeAndRead(want, bufs)
 
 }
 
