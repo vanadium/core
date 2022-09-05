b61ffd6e1188c68b7c099a840ca78e6669e59070
Cosmos Nicolaou <cnicolaou@cloudeng.io>
2022-09-05 15:48:01 -0700

    x/ref/runtime/protocols/lib/framer: bound memory usage (#302)
    This PR ensures that the framer implementation will never use more than a fixed amount of memory. The previous implementation would always copy the supplied data to a newly allocated buffer in order to prepend the frame size before issuing a single system call to write the data. The new implementation instead will only do this for small payloads, for larger ones it will issue multiple system calls, one for the message size and then one per payload argument. This also reduces the amount of data copying which should be beneficial for large payloads.

--- x/ref/runtime/protocols/lib/framer/framer.go
+++ x/ref/runtime/protocols/lib/framer/framer.go
@@ -2,6 +2,9 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// Package framer provides the ability to wrap a network connection
+// (or any io.ReadWriteCloser) with framing that consists of a 3-byte
+// length followed by data of that length.
 package framer
 
 import (
@@ -10,86 +13,107 @@ import (
 	"v.io/v23/flow"
 )
 
-// framer is a wrapper of io.ReadWriter that adds framing to a net.Conn
-// and implements flow.MsgReadWriteCloser.
+const (
+	sizeBytes     = 3
+	maxPacketSize = 0xffffff
+	copyBound     = 8192
+)
+
 type framer struct {
-	io.ReadWriteCloser
-	buf   []byte
-	frame [3]byte
+	rwc        io.ReadWriteCloser
+	readFrame  [sizeBytes]byte
+	writeFrame [sizeBytes]byte
+	writeBuf   []byte
 }
 
+// New creates a new framer instance that implements flow.MsgReadWriteCloser.
+// The framer may issue multiple writes to the underlying connection
+// for a single message. For smaller messages it will copy the data into
+// a single buffer and issue a single write. This combinded approach ensures
+// that the framer has a fixed and known memory overhead.
 func New(c io.ReadWriteCloser) flow.MsgReadWriteCloser {
-	return &framer{ReadWriteCloser: c}
+	f := &framer{
+		rwc:      c,
+		writeBuf: make([]byte, copyBound),
+	}
+	return f
 }
 
+// WriteMsg implements flow.MsgReadWriteCloser. The supplied data may be written
+// to the underlying connection in one or more writes depending on the size
+// of supplied data.
 func (f *framer) WriteMsg(data ...[]byte) (int, error) {
 	// Compute the message size.
 	msgSize := 0
 	for _, b := range data {
 		msgSize += len(b)
 	}
-	// Construct a buffer to write that has space for the 3 bytes of framing.
-	// If a previous buffer is large enough, reuse it.
-	bufSize := msgSize + 3
-	if bufSize > len(f.buf) {
-		f.buf = make([]byte, bufSize)
-	}
-	if err := write3ByteUint(f.buf[:3], msgSize); err != nil {
-		return 0, err
-	}
-	head := 3
-	for _, b := range data {
-		l := len(b)
-		copy(f.buf[head:head+l], b)
-		head += l
-	}
-	// Write the buffer to the io.ReadWriter. Remove the frame size
-	// from the returned number of bytes written.
-	n, err := f.Write(f.buf[:bufSize])
-	if err != nil {
+	if msgSize < (copyBound - sizeBytes) {
+		write3ByteUint(f.writeBuf[:sizeBytes], msgSize)
+		head := sizeBytes
+		for _, d := range data {
+			l := len(d)
+			copy(f.writeBuf[head:head+l], d)
+			head += l
+		}
+		n, err := f.rwc.Write(f.writeBuf[:head])
 		return n - 3, err
 	}
-	return n - 3, nil
+	if msgSize > maxPacketSize {
+		return 0, ErrLargerThan3ByteUInt.Errorf(nil, "integer too large to represent in 3 bytes")
+	}
+	write3ByteUint(f.writeFrame[:], msgSize)
+	if n, err := f.rwc.Write(f.writeFrame[:]); err != nil {
+		return n, err
+	}
+	written := 0
+	for _, d := range data {
+		n, err := f.rwc.Write(d)
+		if err != nil {
+			return written + n, err
+		}
+		written += n
+	}
+	return written, nil
 }
 
-// ReadMsg implements flow.MsgReadWriteCloser
+// ReadMsg implements flow.MsgReadWriteCloser.
 func (f *framer) ReadMsg() ([]byte, error) {
 	return f.ReadMsg2(nil)
+}
+
+// Close implements flow.MsgReadWriteCloser.
+func (f *framer) Close() error {
+	return f.rwc.Close()
 }
 
 // ReadMsg2 implements flow.MsgReadWriteCloser and will use
 // the supplied msg buffer if it is large enough.
 func (f *framer) ReadMsg2(msg []byte) ([]byte, error) {
 	// Read the message size.
-	if _, err := io.ReadFull(f, f.frame[:]); err != nil {
+	if _, err := io.ReadFull(f.rwc, f.readFrame[:]); err != nil {
 		return nil, err
 	}
-	msgSize := read3ByteUint(f.frame)
+	msgSize := read3ByteUint(f.readFrame)
 
 	// Read the message.
 	if msgSize > len(msg) {
 		msg = make([]byte, msgSize)
 	}
 	used := msg[:msgSize]
-	if _, err := io.ReadFull(f, used); err != nil {
+	if _, err := io.ReadFull(f.rwc, used); err != nil {
 		return nil, err
 	}
 	return used, nil
 }
 
-const maxPacketSize = 0xffffff
-
-func write3ByteUint(dst []byte, n int) error {
-	if n > maxPacketSize || n < 0 {
-		return ErrLargerThan3ByteUInt.Errorf(nil, "integer too large to represent in 3 bytes")
-	}
+func write3ByteUint(dst []byte, n int) {
 	n = maxPacketSize - n
 	dst[0] = byte((n & 0xff0000) >> 16)
 	dst[1] = byte((n & 0x00ff00) >> 8)
 	dst[2] = byte(n & 0x0000ff)
-	return nil
 }
 
-func read3ByteUint(src [3]byte) int {
+func read3ByteUint(src [sizeBytes]byte) int {
 	return maxPacketSize - (int(src[0])<<16 | int(src[1])<<8 | int(src[2]))
 }
--- x/ref/runtime/protocols/lib/framer/framer_test.go
+++ x/ref/runtime/protocols/lib/framer/framer_test.go
@@ -6,25 +6,49 @@ package framer
 
 import (
 	"bytes"
+	"crypto/rand"
+	"io"
 	"testing"
 )
 
 type readWriteCloser struct {
-	bytes.Buffer
+	rw    *bytes.Buffer
+	wrops int
 }
 
-func (*readWriteCloser) Close() error {
+func (rwc *readWriteCloser) Write(data []byte) (int, error) {
+	rwc.wrops++
+	return rwc.rw.Write(data)
+}
+func (rwc *readWriteCloser) Read(buf []byte) (int, error) {
+	return rwc.rw.Read(buf)
+}
+
+func (rwc *readWriteCloser) Close() error {
 	return nil
 }
 
-func TestFramer(t *testing.T) {
-	f := &framer{ReadWriteCloser: &readWriteCloser{}}
+func randomBuffer(size int) []byte {
+	b := make([]byte, size)
+	io.ReadFull(rand.Reader, b)
+	return b
+}
 
-	writeAndRead := func(want []byte, bufs [][]byte) {
+func TestFramer(t *testing.T) {
+	rwc := &readWriteCloser{rw: &bytes.Buffer{}}
+	f := New(rwc)
+
+	writeAndRead := func(want []byte, bufs [][]byte, nops int) {
+		rwc.wrops = 0
 		l := len(want)
 		if n, err := f.WriteMsg(bufs...); err != nil || n != l {
 			t.Fatalf("got %v, %v, want %v, nil", n, err, l)
 		}
+		if got, want := rwc.wrops, nops; got != want {
+			t.Errorf("got %v, want %v", got, want)
+		}
+		rwc.wrops = 0
+
 		if got, err := f.ReadMsg(); err != nil || !bytes.Equal(got, want) {
 			t.Errorf("got %v, %v, want %v, nil", got, err, want)
 		}
@@ -47,32 +71,27 @@ func TestFramer(t *testing.T) {
 	bufs := [][]byte{[]byte("read "), []byte("this "), []byte("please.")}
 	want := []byte("read this please.")
 
-	writeAndRead(want, bufs)
+	writeAndRead(want, bufs, 1)
 
-	// Framing a smaller message afterwards should reuse the internal buffer
-	// from the first sent message.
-	oldBufferLen := len(want) + 3
-	bufs = [][]byte{[]byte("read "), []byte("this "), []byte("too.")}
-	want = []byte("read this too.")
+	rnd := randomBuffer(1024)
+	writeAndRead(rnd, [][]byte{rnd}, 1)
 
-	writeAndRead(want, bufs)
+	rnd = randomBuffer(copyBound - 4)
+	writeAndRead(rnd, [][]byte{rnd}, 1)
 
-	if len(f.buf) != oldBufferLen {
-		t.Errorf("framer internal buffer should have been reused")
-	}
-	// Sending larger message afterwards should work as well.
-	bufs = [][]byte{[]byte("read "), []byte("this "), []byte("way bigger message.")}
-	want = []byte("read this way bigger message.")
+	rnd = randomBuffer(copyBound - 1)
+	writeAndRead(rnd, [][]byte{rnd}, 2)
 
-	writeAndRead(want, bufs)
+	rnd = randomBuffer(copyBound - 3)
+	writeAndRead(rnd, [][]byte{rnd}, 2)
 
+	rnd = randomBuffer(copyBound + 10)
+	writeAndRead(rnd, [][]byte{rnd}, 2)
 }
 
 func Test3ByteUint(t *testing.T) {
 	var b [3]byte
-	if err := write3ByteUint(b[:], 65555); err != nil {
-		t.Error(err)
-	}
+	write3ByteUint(b[:], 65555)
 	if got := read3ByteUint(b); got != 65555 {
 		t.Errorf("got %v, want %v", got, 65555)
 	}
