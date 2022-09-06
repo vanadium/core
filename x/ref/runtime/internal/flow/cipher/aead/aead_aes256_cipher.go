cac5d66da71eb32976a08d3b6eba5128a1b25da8
Cosmos Nicolaou <cnicolaou@cloudeng.io>
2022-09-06 09:13:48 -0700

    .

--- x/ref/runtime/internal/flow/cipher/aead/aead_aes256_cipher.go
+++ x/ref/runtime/internal/flow/cipher/aead/aead_aes256_cipher.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package aead uses the aead.GCM block cipher
+// Package aead uses the aead.GCM block cipher with AES 256 encryption.
 package aead
 
 import (
